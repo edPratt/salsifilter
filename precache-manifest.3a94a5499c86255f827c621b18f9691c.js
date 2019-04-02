0db2b2911d4bd8934ab4cd49fb6377f241929e20
Eddie Pratt <epratt8181@gmail.com>
2019-04-01 20:22:54 -0400

    Updates

---
+++ precache-manifest.3a94a5499c86255f827c621b18f9691c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a99636ad2fc26ce66cf1",
+    "url": "/salsifilter/static/js/runtime~main.eb9e74f7.js"
+  },
+  {
+    "revision": "776cc1bc3480bc925cae",
+    "url": "/salsifilter/static/js/main.c06e124b.chunk.js"
+  },
+  {
+    "revision": "c3e7694e0fd130b4921e",
+    "url": "/salsifilter/static/js/2.13dfec23.chunk.js"
+  },
+  {
+    "revision": "776cc1bc3480bc925cae",
+    "url": "/salsifilter/static/css/main.5f9600b5.chunk.css"
+  },
+  {
+    "revision": "f90c38ce8c1f0e37b12060d319ad9e9b",
+    "url": "/salsifilter/index.html"
+  }
+];
